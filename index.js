ce681c2dc2c438e6e21cc8ce13a046e976039b21
Sabrina Carlos Costa <costa.sabrina@academico.ifpb.edu.br>
2021-09-22 18:33:11 +0000

    Criando API  com express

--- index.js
+++ index.js
@@ -12,15 +12,3 @@ app.listen(3000, ()=> {
 
 
 
-/*const express = require("express");
-
-const app = express();
-
-
-app.get("/", (req, res) => {
-  res.json({message:"API KAHI"})
-});
-
-app.listen(3000, ()=> {
-  console.log("API executando!");
-});*/
