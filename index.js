ac45c1c16bf61645640e73ee683a4900b08c806e
bruna.fernanda <bruna.fernanda@academico.ifpb.edu.br>
2021-10-14 01:19:37 +0000

    Recriando tabelas, adicionando relacionamentos

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const db = require("./db")
+const db = require("./db");
 const Usuario = require("./model/Usuario");
 const Exercicio = require("./model/Exercicio");
 const Conteudo = require("./model/Conteudo");
-const Subtopico = require("./model/Subtopico");
 const Topico = require("./model/Topico");
 
-/*const express = require("express");
+const express = require("express");
 
 const app = express()
 
@@ -13,69 +12,74 @@ app.get("/", (req, res) => {
 res.json({message:"API do projeto KAHI, FINALMENTEEEEEEE!"})
 });
 
-app.listen(3000, ()=> {
+/*app.listen(3000, ()=> {
    console.log("API executando!");
-<<<<<<< HEAD
+
 });*/
 
-async function sync() { 
-  await db.sync(); //{force; true}
+//{force; true} {alter:true} livro.drop(); apagar determinada tabela await sequelize.drop();
+/*async function sincronizar() { 
+  await db.sync({force: true})
 }
+sincronizar();
+*/
 
-async function conectar() {
+/*async function conectar() {
   try {
     await db.authenticate();
     console.log("Sucesso!");
   } catch (e) {
     console.log(e, "Falhou!");
   }
-}
+}*/
 
-async function inserirUsuario() {
+ /* async function inserirUsuario() {
   try {
     let usuario1 = {
-      email: "bruna.sabrina@ifpb.com",
-      username: "hihi_muitolindas", senha: "senha123"
+      email: "teste@gmail.com",
+      username:"teste_t",
+      senha: "testando"
     };
 
     await Usuario.create(usuario1);
   } catch (e) {
     console.log(e, "Falhou!")
   }
-}
-  async function listarUsuarios() {
+ }*/
+
+  /*async function listarUsuarios() {
     try {
       let usuarios = await Usuario.findAll();
       console.log(usuarios);
     } catch (e) {
       console.log(e);
     }
-  }
+  }*/
 
-async function adicionarConteudo() {
+/*async function adicionarConteudo() {
   try {
     let conteudo1 = {
-      traducao: "if = se",
-      curiosidade: "bla bla bla bla",
-      instrucoes: "A condicional if é uma estrutura condicional que executa a afirmação, dentro do bloco, se determinada condição for verdadeira.",
-      exemplo_uso: " if(media>= 70) console.log( Aprovado! )"
-    };
+      nome_conteudo: "declarando variaveis",
+      traducao: "aqui colocamos a traducao",
+      instrucoes: "blablablabla",
+      exemplo_uso: " int a= 0;"
+      };
         
     await Conteudo.create(conteudo1);
   } catch (e) {
     console.log(e, "Falhou!")
   }
-}
-async function listarConteudo() {
+}*/
+/*async function listarConteudo() {
     try {
       let conteudos = await Conteudo.findAll();
       console.log(conteudos);
     } catch (e) {
       console.log(e);
     }
-}
+}*/
 
-async function adicionarExercicio() {
+/*async function adicionarExercicio() {
   try {
     let exercicio1 = {
       numeroDoExercicio: "5",
@@ -87,39 +91,20 @@ async function adicionarExercicio() {
   } catch (e) {
     console.log(e, "Falhou!")
   }
-}
-  async function listarExercicio() {
+}*/
+  /*async function listarExercicio() {
     try {
       let exercicios = await Exercicio.findAll();
       console.log(exercicios);
     } catch (e) {
       console.log(e);
     }
-  }
-  async function adicionarSubtopico() {
-  try {
-    let subtopico1 = {
-      nome_subtopico: "If",
-      num_subtopico: "2"
-    };
+  }*/
 
-    await Subtopico.create(subtopico1);
-  } catch (e) {
-    console.log(e, "Falhou!")
-  }
-}
-  async function listarSubtopico() {
-    try {
-      let subtopicos = await Subtopico.findAll();
-      console.log(subtopicos);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-async function adicionarTopico() {
+/*async function adicionarTopico() {
   try {
     let topico1 = {
-      nome_topico: "Introducao Basica",
+      nome_topico: "Conceitos básicos",
       num_topico: "1"
     };
 
@@ -127,7 +112,8 @@ async function adicionarTopico() {
   } catch (e) {
     console.log(e, "Falhou!")
   }
-}
+}*/
+/*
   async function listarTopico() {
     try {
       let topicos = await Topico.findAll();
@@ -135,9 +121,9 @@ async function adicionarTopico() {
     } catch (e) {
       console.log(e);
     }
-  }
+  }*/
 
-  
+
 //conectar();
 //inserirUsuario();
 //sync();
@@ -146,10 +132,144 @@ async function adicionarTopico() {
 //listarConteudo();
 //adicionarExercicio();
 //listarExercicio();
-//adicionarSubtopico();
-//listarSubtopico();
 //adicionarTopico();
 //listarTopico();
+
+
+//consultas funcionando!!
+/*
+const{Op} = require("sequelize");
+
+async function buscarTopico() {
+  let topico = await Topico.findByPk(1);
+console.log(topico);
+}
+buscarTopico();
+*/
+/*
+const{Op} = require("sequelize");
+
+async function buscarUsuario() {
+  let usuario = await Usuario.findAll({
+  where: {
+    id: {
+    [Op.lt]: [2]
+  }
+}
+});
+console.log(usuario);
+}
+buscarUsuario();
+
+
+const{Op} = require("sequelize");
+
+async function retornarUsuarios() {
+  let usuarios1 = await Usuario.findAll({
+  where: {
+    id: {
+    [Op.or]: [1, 3]
+  }
+}
+});
+console.log(usuarios1);
+}
+retornarUsuarios();
+
+ 
+const{Op} = require("sequelize");
+
+async function topicoEmQue() {
+  let numerodotopico = await Topico.findAll({
+  where: {
+   num_topico: '1'
+  }
+})
+console.log(numerodotopico);
+}
+topicoEmQue();
+
+const{Op} = require("sequelize");
+
+async function buscarUsername() {
+  let username1 = await Usuario.findAll({
+  where: {
+    username: 'fer_bruna'
+  }
+});
+console.log(username1);
+}
+buscarUsername();
+
+
+const{Op} = require("sequelize");
+
+async function usuarioEsp() {
+  let usuario = await Usuario.findByPk(2);
+console.log(usuario);
+}
+usuarioEsp();
+
+
+const{Op} = require("sequelize");
+
+async function topicofiltrar() {
+  let topico = await Topico.findAll({
+  where: {
+    nome_topico: 'Conceitos básicos'
+  }
+});
+console.log(topico);
+}
+topicofiltrar();
+
+
+const{Op} = require("sequelize");
+
+async function usuarioFiltrar() {
+  let usuariof = await Usuario.findAll({
+  where: {
+      id: {
+        [Op.between]: [1, 5]
+      }
+  }
+  });
+  console.log(usuariof);
+}
+usuarioFiltrar();
+
+
+const{Op} = require("sequelize");
+
+async function buscarConteudo() {
+  let conteudo = await Conteudo.findAll({
+    where: {
+      id: {
+        [Op.gte]: 1,
+      }
+    }
+  });
+  console.log(conteudo);  
+}
+
+buscarConteudo();
+
+
+
+const{Op} = require("sequelize");
+
+async function filtrarConteudo() {
+  let filconteudos = await Conteudo.findAll({
+    where: {
+      id: {
+        [Op.gte]: 1,
+      }
+    }
+  });
+  console.log(filconteudos);  
+}
+
+filtrarConteudo();*/
 
 
 
--- model/Conteudo.js
+++ model/Conteudo.js
@@ -1,32 +1,47 @@
 let Sequelize = require("sequelize");
 
 const db = require("../db");
-
+const Usuario = require("./Usuario");
 const Conteudo = db.define("Conteudo",
+
       {
         id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
         autoIncrement: true
       },
-         
-        traducao: {
+      
+        nome_conteudo: {
         type: Sequelize.STRING,
+        allowNull: false
+      },
+        
+        traducao: {
+        type: Sequelize.STRING
         
       },
          
         curiosidade: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       
       },
 
         instrucoes: {
         type: Sequelize.STRING,
+        allowNull: false
        
       },
+      
         exemplo_uso: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
         }
    }
 );
+//estuda
+Conteudo.belongsToMany(Usuario, {through:"ConteudoUsuario", as:'usuarios'});Usuario.belongsToMany(Conteudo, {through:"ConteudoUsurio", as: 'conteudos'})
+
+/*Necessario fazer novamente para o relacionamento "pesquisa" entre usuario e conteudo?
+Conteudo.belongsToMany(Usuario, {through:"ConteudoUsuario", as:'usuarios'});Usuario.belongsToMany(Conteudo, {through:"ConteudoUsurio", as: 'conteudos'})*/
+
 module.exports = Conteudo;
